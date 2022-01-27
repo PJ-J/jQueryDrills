1c3e36866db185c7f4331f2520b05b6386a04469
PJ Jones <pj.jones4000.com>
2022-01-27 12:49:53 -0600

    working on making the function add new li instead of adding to existing

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ $(function () {
 
   let div = $("<div>");
   let h2 = $("<h2>");
-  let inputText = $("#inputText").val();
+  let ul = $("<ul>");
+  let li = $("<li>");
 
-  div.appendTo('body');
+  div.appendTo("body");
+  ul.appendTo("body");
 
   $("#btnSubmit").prop("disabled", true);
   $("#inputText").keyup(function () {
@@ -14,10 +16,28 @@ $(function () {
 
   $("#btnSubmit").on("click", function (event) {
     event.preventDefault();
-    
-    h2.append(inputText);
-    div.append(h2);
+
+    // h2.append($("#inputText").val());
+    // div.append(h2);
+
+    // the problem here now is that every time i click submit it just adds text to the already existing li element. i need to make the function create a new li every click
+
+    li.append($("#inputText").val());
+    ul.append(li);
+  });
+
+
+  h2.on("mouseover", function() {
+    $(this).css("background-color", "red");
+    $(this).css("border-radius", "10px");
+  });
+
+ 
+  let randColor = ("rgb("+Math.floor(Math.random() * 255)+","+Math.floor(Math.random() * 255)+","+Math.floor(Math.random() * 255)+")");
+  
+  li.on("click", function() {
+
+    $(this).css("color", randColor);
+    $(this).css("border-radius", "10px");
   });
 });
-
-
